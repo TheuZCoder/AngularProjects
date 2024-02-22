8cd3e1255692fa8568862e8de9e81320599f69e7
TheuZCoder <matheusrdsilva1@gmail.com>
2024-02-22 09:38:12 -0300

    criação view login e movimentação do footer e header para pasta componentes

--- SiteMassoterapia/src/app/app-routing.module.ts
+++ SiteMassoterapia/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './View/home/home.component';
+import { LoginComponent } from './View/login/login.component';
 
 const routes: Routes = [
 
   {
     path: '',
     component: HomeComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
   },
 
 ];
--- SiteMassoterapia/src/app/app.module.ts
+++ SiteMassoterapia/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './View/header/header.component';
-import { FooterComponent } from './View/footer/footer.component';
+import { HeaderComponent } from './Componentes/header/header.component';
+import { FooterComponent } from './Componentes/footer/footer.component';
 import { HomeComponent } from './View/home/home.component';
+import { LoginComponent } from './View/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
