0c5ca4779b6d7693bd3e25cd2902d6e3061ecc08
TheuZCoder <matheusrdsilva1@gmail.com>
2024-04-24 18:25:05 -0300

    Create: Adicionado validador CEP para auto completar input de endereço

--- PizarriaSQL/src/app/view/cliente-cadastro/cliente-cadastro.component.ts
+++ PizarriaSQL/src/app/view/cliente-cadastro/cliente-cadastro.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Cliente } from 'src/app/model/cliente.model';
@@ -6,15 +7,26 @@ import { CadastroClienteService } from 'src/app/service/cadastro-cliente.service
 @Component({
   selector: 'app-cliente-cadastro',
   templateUrl: './cliente-cadastro.component.html',
-  styleUrls: ['./cliente-cadastro.component.css']
+  styleUrls: ['./cliente-cadastro.component.css'],
 })
 export class ClienteCadastroComponent {
-  cliente: Cliente = new Cliente(0,'','','','','');
+  cliente: Cliente = new Cliente(0, '', '', '', '', '');
+  cep: string = '';
 
-  constructor(private clienteService: CadastroClienteService,private router: Router,) {}
+  constructor(
+    private clienteService: CadastroClienteService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   cadastroCliente(): void {
-    if (this.cliente.nome_cliente && this.cliente.email_cliente && this.cliente.endereco_cliente && this.cliente.telefone_cliente && this.cliente.senha_cliente) {
+    if (
+      this.cliente.nome_cliente &&
+      this.cliente.email_cliente &&
+      this.cliente.endereco_cliente &&
+      this.cliente.telefone_cliente &&
+      this.cliente.senha_cliente
+    ) {
       this.clienteService.cadastrarCliente(this.cliente).subscribe(
         () => {
           this.router.navigate(['loginCliente']);
@@ -26,6 +38,24 @@ export class ClienteCadastroComponent {
       );
     } else {
       alert('Por favor, preencha todos os campos obrigatórios.');
+    }
+  }
+
+  buscarEnderecoPorCEP(): void {
+    if (this.cep.length === 8) {
+      this.http
+        .get<any>(`https://viacep.com.br/ws/${this.cep}/json/`)
+        .subscribe(
+          (data) => {
+            this.cliente.endereco_cliente = `${data.logradouro}, ${data.bairro}`;
+          },
+          (error) => {
+            console.error('Erro ao buscar endereço:', error);
+            alert(
+              'Erro ao buscar endereço. Por favor, verifique o CEP e tente novamente.'
+            );
+          }
+        );
     }
   }
 }
