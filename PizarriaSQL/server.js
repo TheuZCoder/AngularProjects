5337e92a5d7e330463d3a2585f3e4bfbc6bdd44d
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-09 19:35:41 -0300

    att server.js adicionado env port

--- PizarriaSQL/server.js
+++ PizarriaSQL/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser"); // Importe o body-parser aqui
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 // Configurar o CORS para permitir todas as origens
 app.use(cors());
