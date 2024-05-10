0237255103ccdef42eada52b79c86adeabae110b
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-10 15:45:07 -0300

    Create: Criado as rotas para todas as paginas

--- ConcessionariaCarro/src/app/app.routes.ts
+++ ConcessionariaCarro/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { HomeComponent } from './view/home/home.component';
 import { CarrosDisponiveisComponent } from './view/carros-disponiveis/carros-disponiveis.component';
+import { MeuAluguelComponent } from './view/meu-aluguel/meu-aluguel.component';
+import { LoginComponent } from './view/login/login.component';
+import { CadastroComponent } from './view/cadastro/cadastro.component';
 
 export const routes: Routes = [
 
@@ -14,5 +17,17 @@ export const routes: Routes = [
     {
         path: 'carrosDisponiveis',
         component: CarrosDisponiveisComponent
+    },
+    {
+        path: 'meuAluguel',
+        component: MeuAluguelComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'cadastro',
+        component: CadastroComponent
     }
 ];
---
+++ ConcessionariaCarro/src/app/view/meu-aluguel/meu-aluguel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-aluguel',
+  standalone: true,
+  imports: [],
+  templateUrl: './meu-aluguel.component.html',
+  styleUrl: './meu-aluguel.component.css'
+})
+export class MeuAluguelComponent {
+
+}
