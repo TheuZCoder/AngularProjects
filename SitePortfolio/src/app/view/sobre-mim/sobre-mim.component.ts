2043d0fa9a1bb7246bfa0266e6dd27d060a3502a
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-13 08:37:52 -0300

    Create: Começado pagina de projetos

--- SitePortfolio/src/app/view/sobre-mim/sobre-mim.component.ts
+++ SitePortfolio/src/app/view/sobre-mim/sobre-mim.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sobre-mim',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./sobre-mim.component.css']
 })
 export class SobreMimComponent {
-
+  
 }
+
+
+
