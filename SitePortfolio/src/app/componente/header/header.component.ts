9a7f5d7b1f2864bd61d37f2194185d85a87fc8ed
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-22 07:58:29 -0300

    Fix:Adiciona Responsividade ao Componente Header

--- SitePortfolio/src/app/componente/header/header.component.ts
+++ SitePortfolio/src/app/componente/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  menuActive = false;
 
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+  }
 }
