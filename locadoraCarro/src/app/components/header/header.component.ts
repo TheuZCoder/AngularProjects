2627e868236f5ba7128b372121336d7f714a40a8
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-24 20:30:25 -0300

    Fix: Adicionado Logica para logout e aparecer meus-alugueis apenas quando cliente estiver logado

--- locadoraCarro/src/app/components/header/header.component.ts
+++ locadoraCarro/src/app/components/header/header.component.ts
@@ -1,20 +1,28 @@
 import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { ClienteService } from 'src/app/service/cliente.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  constructor(private el: ElementRef,
-    private renderer: Renderer2) { }
-    
-    toggleMenu() {
-      const menu = this.el.nativeElement.querySelector('.menu');
-      if (menu.classList.contains('open')) {
-        this.renderer.removeClass(menu, 'open');
-      } else {
-        this.renderer.addClass(menu, 'open');
-      }
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    public clienteService: ClienteService
+  ) {}
+
+  toggleMenu() {
+    const menu = this.el.nativeElement.querySelector('.menu');
+    if (menu.classList.contains('open')) {
+      this.renderer.removeClass(menu, 'open');
+    } else {
+      this.renderer.addClass(menu, 'open');
     }
+  }
+
+  logout() {
+    this.clienteService.logoutCliente();
+  }
 }
